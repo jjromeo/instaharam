9dc5341c358e1997c7baad6dd8ed8e032efd9fc2
Jerome Pratt <jerome.pratt@outlook.com>
2014-10-06 21:44:45 +0100

    fixed bug where all likes on page were being changed by nesting .likes-count

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,9 +1,11 @@
 class LikesController < ApplicationController
 
+include ActionView::Helpers::TextHelper
+
 def create
 	@posts = Post.find(params[:post_id])
 	@posts.likes.create
-	render json: {new_like_count: @posts.likes.count }
+	render json: {new_like_count: "#{pluralize(@posts.likes.count, 'like')}" }
 end
 
 end
