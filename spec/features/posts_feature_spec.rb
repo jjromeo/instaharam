d8f6edfe32d0e01777fa50a41924d5a7c668b663
Jerome Pratt <jerome.pratt@outlook.com>
2014-11-24 14:44:13 +0000

    added a test for adding hashtags to posts

--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -5,29 +5,29 @@ include SessionHelper
 include PostHelper
 
 describe 'posts' do 
-	context 'no posts have been added' do 
+    context 'no posts have been added' do 
         before do
             sign_up
         end
-		it 'should display a prompt to add pictures' do 
-			visit '/posts'
-			expect(page).to have_content 'No posts yet'
-			expect(page).to have_link 'Add a post'
-		end
-	end
+        it 'should display a prompt to add pictures' do 
+            visit '/posts'
+            expect(page).to have_content 'No posts yet'
+            expect(page).to have_link 'Add a post'
+        end
+    end
 
-	context 'posts have been added' do 
-		before do 
+    context 'posts have been added' do 
+        before do 
             sign_up
             add_post
-		end
+        end
 
-		it 'should display posts' do 
-			visit '/posts'
-			expect(page).to have_content('So much fun!')
-			expect(page).not_to have_content('No posts yet')
-		end
-	end
+        it 'should display posts' do 
+            visit '/posts'
+            expect(page).to have_content('So much fun!')
+            expect(page).not_to have_content('No posts yet')
+        end
+    end
 end
 
 
@@ -37,16 +37,16 @@ describe 'adding a post' do
             sign_up
         end
 
-		it 'prompts user to fill in a form and then displays post' do
+        it 'prompts user to fill in a form and then displays post' do
             add_post
-			expect(page).to have_content 'So much fun!'
-		end
+            expect(page).to have_content 'So much fun!'
+        end
 
         it 'displays the name of the user who posted it' do 
             add_post
             expect(page).to have_content 'by tester'
         end
-	end
+    end
     
     context 'when not signed in' do
         it 'does not let you post' do 
@@ -54,6 +54,20 @@ describe 'adding a post' do
             expect(page).not_to have_content "Add a post"
         end
     end
+end
+
+describe 'adding hashtags to posts' do 
+        it 'creates a clickable hashtag when you add a caption with hashtags' do
+            sign_up
+            visit '/posts'
+            add_post("I am #super cool")
+            add_post("I am #super awesome")
+            add_post("Not super anymore")
+            click_link "#super"
+            expect(page).to have_content "I am #super cool"
+            expect(page).to have_content "I am #super awesome"
+            expect(page).not_to have_content "Not super anymore"
+        end
 end
 
 
