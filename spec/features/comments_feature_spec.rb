920f464d4e7c574cc3f419c75d7b7c8863de8e8c
Jerome Pratt <jerome.pratt@outlook.com>
2015-01-03 17:14:21 +0000

    added test for adding hashtags to comments

--- spec/features/comments_feature_spec.rb
+++ spec/features/comments_feature_spec.rb
@@ -1,30 +1,42 @@
-    require 'rails_helper'
+require 'rails_helper'
 
-    describe 'comments' do 
+describe 'comments' do 
 
-        it "allows users to add a comment to a post" do 
-            sign_up
-            add_post
-            visit '/posts'
-            click_link "Comment!"
-            fill_in "Text", with: "Amazing testing!"
-            click_button 'send'
-            expect(page).to have_content "Amazing testing!"
-        end
+  it "allows users to add a comment to a post" do 
+    sign_up
+    add_post
+    visit '/posts'
+    click_link "Comment!"
+    fill_in "Text", with: "Amazing testing!"
+    click_button 'send'
+    expect(page).to have_content "Amazing testing!"
+  end
 
-        it 'mentions who posted it' do 
-            sign_up
-            add_post
-            click_link 'Sign out'
-            sign_up('jerome@test.com', 'jjromeo')
-            visit '/posts'
-            click_link "Comment!"
-            fill_in "Text", with: "Amazing testing!"
-            click_button 'send'
-            expect(page).to have_content "by jjromeo"
-        end
-        
+  it 'mentions who posted it' do 
+    sign_up
+    add_post
+    click_link 'Sign out'
+    sign_up('jerome@test.com', 'jjromeo')
+    visit '/posts'
+    click_link "Comment!"
+    fill_in "Text", with: "Amazing testing!"
+    click_button 'send'
+    expect(page).to have_content "by jjromeo"
+  end
+
+  it 'creates clickable hashtags when comments are made' do 
+    sign_up
+    add_post
+    click_link 'Sign out'
+    sign_up('jerome@test.com', 'jjromeo')
+    visit '/posts'
+    click_link "Comment!"
+    fill_in "Text", with: "Amazing #comments" 
+    click_button 'send'
+    click_link '#comments'
+    expect(page).to have_content 'So much fun!'
+  end
 
 
 
-    end
+end
