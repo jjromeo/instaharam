c24bd18352e206cfa436e553d87b26d9e2e4f57c
Jerome Pratt <jerome.pratt@outlook.com>
2015-01-09 22:22:36 +0000

    added like_spec and relation between likes and users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150105230430) do
+ActiveRecord::Schema.define(version: 20150109222200) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 20150105230430) do
     t.integer  "post_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   add_index "likes", ["post_id"], name: "index_likes_on_post_id", using: :btree
---
+++ spec/models/like_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe Like, type: :model do 
+
+  it { should belong_to :post }
+  it { should belong_to :user }
+
+end
